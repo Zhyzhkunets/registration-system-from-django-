dc134d39c6ca551008a0a0dd4b6c28409e12e007
ZhyzhkunetsA <pochtolyon@gmail.com>
2014-10-16 18:14:15 +0300

    wrote models

--- test_task/core/models.py
+++ test_task/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User, UserManager
 
-# Create your models here.
+class CustomUser(User):
+    """User with app settings."""
+    date_birth = models.DateField()
+    phone = models.CharField(max_length=30)
+
+    # Use UserManager to get the create_user method, etc.
+    objects = UserManager()
+
+class Post(models.Model):
+    """This model for a resume"""
+    title = models.CharField(max_length=50)
+    body = models.TextField(max_length=10000)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
