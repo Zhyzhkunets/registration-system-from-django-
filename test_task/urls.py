b868d3a60b8e89e001c421b079114f99e38a2339
ZhyzhkunetsA <pochtolyon@gmail.com>
2014-10-16 19:31:23 +0300

    added app's urls

--- test_task/urls.py
+++ test_task/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('test_task.core.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
