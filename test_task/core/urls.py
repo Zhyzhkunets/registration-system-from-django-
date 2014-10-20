34973debc3f297a32fcb8834c4797de49cf9b55e
ZhyzhkunetsA <pochtolyon@gmail.com>
2014-10-20 23:36:46 +0300

    added log... urls

--- test_task/core/urls.py
+++ test_task/core/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
-from views import home_view
+from test_task.core import views
 
 urlpatterns = patterns('',
-    url(r'^$', home_view, name='home'),  
+    url(r'^$', views.home_view, name='home'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),  
+    url(r'^loggedin/$', views.loggedin, name='loggedin'),
 )
