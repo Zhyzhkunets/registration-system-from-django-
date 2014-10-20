3c0da4a7f9c734236370fb0edaaa9db398064fa0
ZhyzhkunetsA <pochtolyon@gmail.com>
2014-10-21 01:24:51 +0300

    wrote regist view

--- test_task/core/views.py
+++ test_task/core/views.py
@@ -4,16 +4,21 @@ from django.http import HttpResponseRedirect
 # my models
 from models import CustomUser, Post
 
+# my forms
+from forms import CustomUserForm
+
 # security
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
+
 
 # django auth form 
 from django.contrib.auth.forms import AuthenticationForm
 
 # django auth "tools"
 from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
 
 def home_view(request):
     posts = Post.objects.order_by('-id')
@@ -22,7 +27,7 @@ def home_view(request):
 @sensitive_post_parameters()
 @csrf_protect
 @never_cache
-def login(request):
+def login(request): # I think, 'login_view' is better and correct 
 
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -47,4 +52,21 @@ def loggedin(request):
 	}
 
 	return render(request, 'loggedin.html', context)
+
+def registrate_view(request):
+    
+    if request.method == 'POST':
+    	form = CustomUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect('/loggedin/') # redirect must be other
+    else:
+        form = CustomUserForm()
+
+    return render(request, 'registrate.html', {'form':form})
+
+
+
 
