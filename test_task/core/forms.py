c2d3fad5022d7e95a255befee1bd7f46f9a67179
ZhyzhkunetsA <pochtolyon@gmail.com>
2014-10-21 01:26:08 +0300

    regist form

--- test_task/core/forms.py
+++ test_task/core/forms.py
@@ -1,2 +1,11 @@
 from django.forms import ModelForm
 from models import CustomUser
+
+class CustomUserForm(ModelForm):
+
+	class Meta:
+		model = CustomUser
+		fields = ['first_name', 'last_name', 'username', 
+                  'password', 'email', 'date_birth', 'phone'] 
+                 # Need to add "Password confirmation" field
+
